91e6665daf861a4933aa10b9be34277d34e1d9c5
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-05-30 14:57:16 +0200

    Add files via upload

--- neural.py
+++ neural.py
@@ -7,28 +7,31 @@ import os
 import csv
 from sklearn.neural_network import MLPClassifier
 import pickle
+from sklearn.preprocessing import StandardScaler
 
 a = pd.read_csv("n.csv")
 a = pd.DataFrame(a)
-y = np.array(a)
+y = a
+sc = StandardScaler()
+
 
 x_img = []
 
-for nombre in os.listdir(r"dogs-vs-cats\train\train"):
-    images = image.load_img(r"dogs-vs-cats\train\train\\" + nombre, target_size=(28, 28))
+for nombre in os.listdir(r"kagglecatsanddogs_5340\train\train"):
+    images = image.load_img(r"kagglecatsanddogs_5340\train\train\\" + nombre, target_size=(128, 128))
     
     x = image.img_to_array(images)
     x = tf.image.rgb_to_grayscale(x)
     #x = np.expand_dims(x, axis=0)
     x_img.append(x)
 
-x = np.array(x_img).reshape(-1, 28*28)
-
-model = MLPClassifier(hidden_layer_sizes=(100, 100, 100), max_iter=1000)
-model.fit(x, y)
+x = np.array(x_img).reshape(-1, 128*128)
+x = sc.fit_transform(x)
+model = MLPClassifier(hidden_layer_sizes=(128), max_iter=1000)
+model.fit(x, y.values.ravel())
 
 # Save the model to disk
-filename = 'm_test.pkl'
+filename = 'm_test5.pkl'
 pickle.dump(model, open(filename, 'wb'))
 
 
